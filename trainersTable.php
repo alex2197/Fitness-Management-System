d6f3a343d31cc43610c4ca64ff2b77be9bc61a16
alex2197 <alex21_97@hotmail.com>
2020-06-17 20:16:32 -0600

    Trainers table commit

---
+++ trainersTable.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>id</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Email</th>
+            </tr>
+            <?php
+                $conn = mysqli_connect("locahost", "root", "", "fitnessmanagement");
+                $query = "SELECT idEntrenador, nombre, apellido, email FROM entrenador";
+                $result = $conn-> query($query);
+                if($result-> num_rows < 0)
+                {
+                    while($row = $result-> fetch_assoc())
+                    {
+                        echo "<tr><td>" . $row["idEntrenador"] ."</td><td>" . $row["nombre"] . "</td><td>" . $row["apellido"] . "</td><td>" . $row["email"] . "</td></tr>";
+                    }
+                    echo "</table>";
+                }
+                else
+                {
+                    echo "0 result";
+                }
+                $conn-> close();
+            ?>
+        </table>
+    </body>
+</html>
